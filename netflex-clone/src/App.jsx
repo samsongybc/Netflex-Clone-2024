e20493170266231807033624cad68453c080eb3f
samsongybc <samsongybc@gmail.com>
2025-09-05 02:01:30 -0400

    first_commit

---
+++ netflex-clone/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div>Hello</div><br /><br /><div>Heloo how are you</div>
+    </>
+  )
+}
+
+export default App
