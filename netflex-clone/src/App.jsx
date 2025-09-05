171bd3bbf37c0e3ce3cc2be3ef00d71f91ee81fa
samsongybc <samsongybc@gmail.com>
2025-09-05 13:12:29 -0400

    Header and Footer

--- netflex-clone/src/App.jsx
+++ netflex-clone/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
   
 
   return (
     <>
-     <div>Hello</div><br /><br /><div>Heloo how are you</div>
+    <Home/>
     </>
   )
 }
---
+++ netflex-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Footer from '../../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <>
+    {/* <h1>Hello Netflex</h1> */}
+      <Header />
+      <Footer />
+    </>
+  );
+}
+
+export default Home
