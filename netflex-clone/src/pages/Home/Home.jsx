83a7c4da67942edaa18909c80a406b8921fefe41
samsongybc <samsongybc@gmail.com>
2025-09-08 18:22:14 -0400

    axios and request

--- netflex-clone/src/pages/Home/Home.jsx
+++ netflex-clone/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
+import Movies from '../../components/Movies/Movies';
 
 const Home = () => {
   return (
     <>
     {/* <h1>Hello Netflex</h1> */}
       <Header />
+      <Movies/>
       <Footer />
     </>
   );
