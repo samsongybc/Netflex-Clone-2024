496d7f440f5c49044d047237bed0cc83b2d42164
samsongybc <samsongybc@gmail.com>
2025-09-08 21:51:45 -0400

    banner

--- netflex-clone/src/pages/Home/Home.jsx
+++ netflex-clone/src/pages/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer'
 import Movies from '../../components/Movies/Movies';
+import Banner from '../../components/Banner/Banner';
 
 const Home = () => {
   return (
     <>
     {/* <h1>Hello Netflex</h1> */}
       <Header />
-      <Movies/>
+      <Banner/>
+      {/* <Movies/> */}
       <Footer />
     </>
   );
