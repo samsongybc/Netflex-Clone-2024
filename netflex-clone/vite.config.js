19b0e2ff88c27cc5a2e140f584a2dbcb9bf6b116
samsongybc <samsongybc@gmail.com>
2025-09-18 13:23:34 -0400

    final deploy

--- netflex-clone/vite.config.js
+++ netflex-clone/vite.config.js
@@ -1,17 +1,21 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Netflix-Clone-2024/",
+  base: "/Netflex-Clone-2024/",
   build: {
     outDir: "dist",
   },
 });
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/Netflix-Clone-2024/",
+//   build: {
+//     outDir: "dist",
+//   },
+// });
